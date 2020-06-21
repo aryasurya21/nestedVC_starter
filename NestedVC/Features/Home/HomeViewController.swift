c611fb5047cb9eb2d35f01e0a4b574db9393b892
Arya S <arya.surya021@gmail.com>
2020-06-21 15:43:29 +0700

    Setup Startup Project

---
+++ NestedVC/Features/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  NestedVC
+//
+//  Created by IT Division on 21/06/20.
+//  Copyright © 2020 com.it-division. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
